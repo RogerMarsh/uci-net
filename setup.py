767ade608f1511d6d7e5671f014295847a9e441c
Roger Marsh <roger.marsh@solentware.co.uk>
2021-05-02 08:20:27 +0000

    From uci-net-1.2.tar.gz uploaded to solentware.co.uk on 2019-11-11.
    Structure changed: main code moved from uci_net to uci_net/uci_net.

--- setup.py
+++ setup.py
@@ -10,25 +10,21 @@ if __name__ == '__main__':
 
     setup(
         name='uci-net',
-        version='1.1.1',
+        version='1.2',
         description='Universal Chess Interface client-server conversation',
         author='Roger Marsh',
         author_email='roger.marsh@solentware.co.uk',
         url='http://www.solentware.co.uk',
-        package_dir={'uci_net':''},
         packages=[
             'uci_net',
             'uci_net.samples',
-            'uci_net.about',
             ],
-        package_data={
-            'uci_net.about': ['LICENCE', 'CONTACT'],
-            },
         long_description=long_description,
         license='BSD',
         classifiers=[
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Operating System :: OS Independent',
             'Topic :: Software Development',
             'Intended Audience :: Developers',
